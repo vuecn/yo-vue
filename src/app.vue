ac844a7d8603d27c67561144da470d71c2067e96
zanjs <root@zanjs.com>
2016-01-11 18:40:58 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <input type="text" v-model="text" />
+    <div class="hello">Hello2 {{ text }}!</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    replace: false,
+  	data() {
+      return {
+        text: 'vue'
+      }
+    }
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import app from './app.vue'
+
+import './styles.css'
+
+
+
+const App = new Vue({
+  el: 'body',
+  components: {
+    app
+  }
+})
